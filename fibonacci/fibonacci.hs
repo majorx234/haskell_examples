5cfa2b6d69f9362eec83360642ea81220e39023f
MajorX234 <majorx234@googlemail.com>
2022-02-20 14:44:41 +0100

    Add tail recursion example

---
+++ fibonacci/fibonacci.hs
@@ -0,0 +1,13 @@
+fibo :: Integer -> Integer
+fibo 1 = 0
+fibo 2 = 1
+fibo n = fibo (n-1) + fibo (n-2)
+
+fibo_forward :: Integer -> Integer
+fibo_forward 1 = 0
+fibo_forward 2 = 1
+fibo_forward n = fibo_tail (1,2,n)
+
+fibo_tail :: (Integer,Integer,Integer) -> Integer
+fibo_tail (last,lastlast,3) = last
+fibo_tail (last,lastlast,n) = fibo_tail (lastlast,last+lastlast,n-1)
