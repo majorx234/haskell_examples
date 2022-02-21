763c9cff552d138936bce35d4d85eecfb4b1c39a
MajorX234 <majorx234@googlemail.com>
2022-02-22 00:34:03 +0100

    Clean up code

--- haskell_audio/adsr_modificator.hs
+++ haskell_audio/adsr_modificator.hs
@@ -1,32 +1,24 @@
----module Main where
+module Main where
 
-adsr_func :: (Float,Float,Float,Float,Float,Float) -> Float -> Float
-adsr_func (numSamples,at,dt,st,rt,pos) value |pos  < at * numSamples = value * (pos /(at * numSamples)) 
+adsr_func :: (Float,Float,Float,Float,Float) -> Float -> Float -> Float
+adsr_func (numSamples,at,dt,st,rt) pos value |pos  < at * numSamples = value * (pos /(at * numSamples)) 
                                              |pos  < (at + dt) * numSamples = value * (1 - (0.7 * (pos - at * numSamples))/(dt * numSamples))
                                              |pos  < (at +dt + st) * numSamples = value * 0.3
                                              |otherwise = value * (0.3 - (0.3 * (pos-(at+dt+st) * numSamples))/(rt * numSamples))
 
----            f [samples] (numSamples)
-adsrmap :: ((Float ,Float,Float,Float,Float,Float) -> a -> b) -> [a]  -> (Float,Float,Float,Float,Float,Float) -> [b]
-adsrmap f [] _ = []
-adsrmap f (x:xs) (numSamples,at,dt,st,rt,pos)  = ((f (numSamples,at,dt,st,rt,pos) x) : (adsrmap f xs (numSamples,at,dt,st,rt,pos + 1)))
+adsrmap :: ((Float ,Float,Float,Float,Float) -> Float -> a -> b) -> [a]  -> (Float,Float,Float,Float,Float) -> Float -> [b]
+adsrmap f [] _ _ = []
+adsrmap f (x:xs) params pos  = ((f params pos x) : (adsrmap f xs params (pos + 1)))
 
-adsrmap_akku :: ([Float],[Float],((Float,Float,Float,Float,Float,Float) -> Float -> Float),(Float,Float,Float,Float,Float,Float)) -> [Float]
+adsrmap_akku :: ([Float],[Float],((Float,Float,Float,Float,Float) -> Float -> Float -> Float),(Float,Float,Float,Float,Float,Float)) -> [Float]
 adsrmap_akku ([],out,_,_) = out
-adsrmap_akku ((x:xs),ys,f,(numSamples,at,dt,st,rt,pos)) |pos == numSamples-1 = ((f (numSamples,at,dt,st,rt,pos) x) : ys)
-                                                         |otherwise = adsrmap_akku (xs,((f (numSamples,at,dt,st,rt,pos) x) : ys),f,(numSamples,at,dt,st,rt,pos+1))
+adsrmap_akku ((x:xs),ys,f,(numSamples,at,dt,st,rt,pos)) |pos == numSamples-1 = ((f (numSamples,at,dt,st,rt) pos x) : ys)
+                                                         |otherwise = adsrmap_akku (xs,((f (numSamples,at,dt,st,rt) pos x) : ys),f,(numSamples,at,dt,st,rt,pos+1))
 
-
-
-
-adsr_modificator (at, dt, st, rt) (numSamples, ls) = reverse $ adsrmap_akku (ls, [], adsr_func, (numSamples,at,dt,st,rt, 0.0)) 
+adsr_modificator :: (Float,Float,Float,Float) -> (Float,[Float]) -> [Float]
+adsr_modificator (at, dt, st, rt) (numSamples, ls) =   reverse $ adsrmap_akku (ls, [], adsr_func, (numSamples,at,dt,st,rt, 0.0))
 
 adsr_wrap :: (Float,Float,Float,Float) -> [Float] -> [Float]
 adsr_wrap param (x:xs) = (x : (adsr_modificator param (x , xs))) 
 
 main = interact $ unlines . map show . (adsr_wrap (0.1, 0.2, 0.4, 0.3)) . map read . words
---- main = interact $ unlines . map show . gen_wave "sin" . map read . words
---- main = unlines $ map show $ gen_wave "sin" $ map read $ words 
---- main = interact $ unlines . map show . gen_wave "sin" . map read . words
---- unline . map show .
----  print $ gen_wave "saw" 200.0 1.0
