25b70227b849b7e38835fa6d0d4df4c45fe2e73e
MajorX234 <majorx234@googlemail.com>
2022-02-25 00:45:23 +0100

    Fix saw wave amplitude, add tri angle wave

--- haskell_audio/wave_gen.hs
+++ haskell_audio/wave_gen.hs
@@ -16,19 +16,29 @@ sin_wave pitch duration fs = (fs * duration , map sin $ map (*step) [0.0, 1 .. f
                                step = (pitch * 2 *pi)/fs
 
 saw_func :: (Fractional a, RealFrac a) => a -> a
-saw_func x = x - fromIntegral (round (x - 0.5))
+saw_func x = 2 * (x - fromIntegral (round (x - 0.5))) - 1
 
 saw_wave :: Float -> Float -> Samples -> (Samples,[Float])
 saw_wave pitch duration fs = (fs * duration , map saw_func $ map (*step) [0.0, 1/fs .. duration-1/fs])
                               where
                                 step = pitch
 
+tri_func :: (Fractional a, RealFrac a) => a -> a
+tri_func x = 4 * abs ((x - 0.25) - fromIntegral (round (x - 0.75)) - 0.5) - 1
+
+tri_wave :: Float -> Float -> Samples -> (Samples,[Float])
+tri_wave pitch duration fs = (fs * duration , map tri_func $ map (*step) [0.0, 1/fs .. duration-1/fs])
+                              where
+                                step = pitch
+
 gen_wave :: String -> [Float] -> [Samples]
 gen_wave wave_form (pitch:duration:_) |wave_form == "sin" = let (x,xs) = sin_wave pitch duration sample_rate in (x:xs)
                                        | wave_form == "saw" = let (x,xs) = saw_wave pitch duration sample_rate in (x:xs)
+                                       | wave_form == "tri" = let (x,xs) = tri_wave pitch duration sample_rate in (x:xs)
                                        |otherwise = (sample_rate * duration : map (0 *)[0.. sample_rate*duration] )
 
 main :: IO ()
 main = do  
     (wave_str:wave_params) <- getArgs
     putStr $ unlines . map show $ gen_wave wave_str (map read wave_params)
+
