49370b89bf9d8e704b19b402585012697fceccbc
MajorX234 <majorx234@googlemail.com>
2022-06-05 23:45:57 +0200

    Add multiply function to matrix example.

--- matrix/matrix.hs
+++ matrix/matrix.hs
@@ -5,3 +5,6 @@ type Matrix a = [[a]]
 transpose :: Matrix a -> Matrix a
 transpose (x:[]) = map (\ v -> (v:[])) x 
 transpose (x:xs) = zipWith (:) x (transpose xs)
+
+multiply :: (Num a) => Matrix a -> Matrix a -> Matrix a
+multiply xm ym = map (\ x_row -> (map (\ y_column -> sum (zipWith (*) x_row y_column)) (transpose ym))) xm
