cac587009c43164841fe85460f40a3cdef72443f
MajorX234 <majorx234@googlemail.com>
2022-06-06 02:22:24 +0200

    Modify multiply function with DataKinds.

--- matrix/matrix.hs
+++ matrix/matrix.hs
@@ -1,10 +1,38 @@
-data Nat = Zero | Succ Nat
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE KindSignatures #-}
 
-type Matrix a = [[a]]
+import Data.Kind (Type)
 
-transpose :: Matrix a -> Matrix a
-transpose (x:[]) = map (\ v -> (v:[])) x 
-transpose (x:xs) = zipWith (:) x (transpose xs)
+data Nat = Zero
+           | Succ Nat
 
-multiply :: (Num a) => Matrix a -> Matrix a -> Matrix a
-multiply xm ym = map (\ x_row -> (map (\ y_column -> sum (zipWith (*) x_row y_column)) (transpose ym))) xm
+data Vec  :: Nat -> * -> * where
+  Nil  :: Vec Zero a
+  (:>) :: a -> Vec n a -> Vec (Succ n) a
+
+type Matrix m n  a = Vec m (Vec n a)
+
+sumi :: (Num a) => Vec n a -> a
+sumi Nil = 0
+sumi (x:>xs) = x + sumi xs
+
+mapi :: (a->b) -> Vec n a -> Vec n b
+mapi f Nil     = Nil
+mapi f (x:>xs) = (f x) :> mapi f xs
+
+zipWithi :: (a->b->c) -> Vec d a -> Vec d b -> Vec d c
+zipWithi f Nil Nil = Nil
+zipWithi f (x:>xs) (y:>ys) = ((f x y) :> (zipWithi f xs ys))
+
+transpose :: Matrix m n a -> Matrix n m a
+transpose (x :> Nil) = mapi (\ v -> (v:>Nil)) x 
+transpose (x :> xs@( y:> ys)) = zipWithi (:>) x (transpose xs)
+
+multiply :: (Num a) => Matrix d d a -> Matrix d d a -> Matrix d d a
+multiply xm ym = mapi (\ x_row -> (mapi (\ y_column -> sumi (zipWithi (*) x_row y_column)) (transpose ym))) xm
