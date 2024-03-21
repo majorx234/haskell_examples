175434aff15ba2b5383803591e7822fc73ae949f
MajorX234 <majorx234@googlemail.com>
2024-03-21 17:08:30 +0100

    Change convertion function to char

--- bin_to_ascii/bin_to_ascii.hs
+++ bin_to_ascii/bin_to_ascii.hs
@@ -4,7 +4,7 @@ import Data.List
 import Data.Function
 import Text.Printf
 import Data.Char (digitToInt)
-import ASCII.Char
+import Data.Char (chr)
 
 convert_bin_to_int :: String -> Int
 convert_bin_to_int = foldr step 0
@@ -14,5 +14,5 @@ convert_bin_to_int = foldr step 0
 main :: IO ()
 main = do
   file <- readFile "./bin.txt"
-  print (map fromIntUnsafe (map convert_bin_to_int (words file)) )
+  print (map chr (map convert_bin_to_int (words file)) )
 
