936c69656120f0a462c71ea4f7a73ffa1964ec75
MajorX234 <majorx234@googlemail.com>
2024-03-22 02:39:42 +0100

    Add a reverse function + second implementation

--- bin_to_ascii/bin_to_ascii.hs
+++ bin_to_ascii/bin_to_ascii.hs
@@ -10,9 +10,14 @@ convert_bin_to_int :: String -> Int
 convert_bin_to_int = foldr step 0
     where step x y = (+) (digitToInt x) ( (*) y 2 )
 
+convert_bin_to_int2 :: String -> Int
+convert_bin_to_int2 [] = 0
+convert_bin_to_int2 (x : xs) = (digitToInt x) + 2 * convert_bin_to_int2 xs
+
+convert_reverse_bin_to_int x = convert_bin_to_int (reverse x)
 
 main :: IO ()
 main = do
   file <- readFile "./bin.txt"
-  print (map chr (map convert_bin_to_int (words file)) )
+  print (map chr (map convert_reverse_bin_to_int (words file)) )
 
