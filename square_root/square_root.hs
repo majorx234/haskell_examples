8eabfc53e983e3636dbf0bc2a0814722a1799c7a
MajorX234 <majorx234@googlemail.com>
2025-06-08 02:55:46 +0200

    Add combined function

--- square_root/square_root.hs
+++ square_root/square_root.hs
@@ -1,6 +1,12 @@
 sqr_aux :: Double -> Double -> Double -> Double -> Double
-sqr_aux x eps h h_old |(h - h_old) * (h - h_old) < eps*eps = h
-                      |otherwise = sqr_aux x eps (0.5*h + 0.5*x/h) h
+sqr_aux a eps h h_old |(h - h_old) * (h - h_old) < eps*eps = h
+                      |otherwise = sqr_aux a eps (0.5*h + 0.5*a/h) h
 
 sqr :: Double -> Double -> Double
-sqr x eps = sqr_aux x eps ((1 + x)/2) (2*x)
+sqr a eps = sqr_aux a eps ((1 + a)/2) (2*a)
+
+
+sqr2 :: Double -> Double -> Double
+sqr2 a eps = sqr_aux a eps ((1 + a)/2) (2*a)
+             where sqr_aux2 a eps h h_old |(h - h_old) * (h - h_old) < eps*eps = h
+                                          |otherwise = sqr_aux2 a eps (0.5*h + 0.5*a/h) h
